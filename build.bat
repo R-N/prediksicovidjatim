aabf5c1f58f273f394631cb4d82860ef782ad408
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-23 13:17:43 +0700

    asd

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 setlocal
 SET VERCEL_BUILDER_DEBUG=1
 SET VERCEL_BUILD_SYSTEM_REPORT=1
+SET NEXT_DEBUG_FUNCTION_SIZE=1
 echo %VERCEL_BUILD_SYSTEM_REPORT%
-vercel --prod --debug
+vercel dev --debug
 endlocal
